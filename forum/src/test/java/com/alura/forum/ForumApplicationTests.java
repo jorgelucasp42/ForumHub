c31474559c7f3e6718ccc7fad2e3304d00357485
Jorge <103595844+jorgelucasp42@users.noreply.github.com>
2024-07-15 19:57:10 -0300

    update

--- forum/src/test/java/com/alura/forum/ForumApplicationTests.java
+++ forum/src/test/java/com/alura/forum/ForumApplicationTests.java
@@ -1,13 +1,12 @@
 package com.alura.forum;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class ForumApplicationTests {
+@SpringBootApplication
+public class ForumApplication {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(ForumApplication.class, args);
 	}
-
 }
